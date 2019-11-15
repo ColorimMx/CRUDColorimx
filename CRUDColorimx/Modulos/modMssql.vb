804b50ab84adf1209dc7f87f15ab012127e2a56e
Pablo Amador <pablo.amador@colorim.com.mx>
2019-11-15 10:47:09 -0600

    Creación de Módulos iniciales
    Modulo 4D, Imgs, Mssql, Salir y Modulo1

---
+++ CRUDColorimx/Modulos/modMssql.vb
@@ -0,0 +1,25 @@
+﻿Imports System.Data
+Imports System.Data.SqlClient
+Public Module modMssql
+    Public sqluser As String = "sa"
+    Public sqlpass As String = "Sql2008"
+    Public sqlserver As String = "100.48.0.88"
+    Public sqldb As String = "4D_COLORANT"
+
+
+    Public conmssql As New SqlConnection("Data Source=" & sqlserver +
+                                         ";Initial Catalog=" & sqldb +
+                                         "; Persist Security Info=True;" +
+                                         "User ID=" & sqluser +
+                                         "; Password=" & sqlpass +
+                                         ";")
+
+    Sub conectarsql()
+        Try
+            conmssql.Open()
+            MsgBox("Conexion exitosa a MSSQL")
+        Catch ex As SqlException
+            MsgBox("Atención!!! " & ex.Message.ToString, MsgBoxStyle.Critical + MsgBoxStyle.OkOnly)
+        End Try
+    End Sub
+End Module
